72c5315a1234e2e21a669d5997fb969871d7c199
rkaiser <ricky.kaiser@cape-it.de>
2017-08-30 11:21:10 +0200

    changed sidebars (placeholder), changed marko indent

--- src/components/mutableContent/kix-sidebar/component.ts
+++ src/components/mutableContent/kix-sidebar/component.ts
@@ -3,11 +3,26 @@ class KIXSidebarComponent {
     public state: any;
 
     public onCreate(input: any): void {
-        this.state = {};
+        this.state = {
+            showNodeSidebar: true,
+            hasRightsForNodeSidebar: true
+        };
     }
 
     public onMount(): void {
         console.log("Mount Sidebar");
+    }
+
+    // function to show/hide given sidebar
+    public toggleSidebar(sidebar: string): void {
+        if (!sidebar) {
+            return;
+        }
+        if (this.state['show' + sidebar]) {
+            this.state['show' + sidebar] = false;
+        } else {
+            this.state['show' + sidebar] = true;
+        }
     }
 }
 
