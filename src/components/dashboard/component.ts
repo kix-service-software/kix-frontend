c2ec62c384dcc85f1ff818e181df3d44c3f15e82
David Hein <david.hein@cape-it.de>
2017-08-31 17:03:12 +0200

    example drag and drop

--- src/components/dashboard/component.ts
+++ src/components/dashboard/component.ts
@@ -4,7 +4,17 @@ class DashboardComponent {
 
     public onCreate(input: any): void {
         this.state = {
-            text: 'Test'
+            text: 'Test',
+            widgets: [
+                "Widget01",
+                "Widget02",
+                "Widget03",
+                "Widget04",
+                "Widget05",
+                "Widget06"
+            ],
+            tempWidgets: [],
+            dragging: false
         };
     }
 
@@ -14,6 +24,71 @@ class DashboardComponent {
 
     public sayHello(): void {
         alert('Hello');
+    }
+
+    public dragStart(event): void {
+        this.state.tempWidgets = Array.from(this.state.widgets);
+        event.dataTransfer.setData("text", event.target.id);
+        event.target.classList.add('drag');
+    }
+
+    public dragOver(event): void {
+        if (event.preventDefault) {
+            // Necessary. Allows us to drop.
+            event.preventDefault();
+        }
+
+        const dropWidgetId = event.target.id;
+        const dragWidgetId = event.dataTransfer.getData("text");
+
+        if ((dropWidgetId === dragWidgetId) ||
+            (dropWidgetId === "" || dragWidgetId === "")) {
+            return;
+        }
+
+        const array = this.state.widgets;
+
+        const dragIndex = array.indexOf(dragWidgetId);
+        array.splice(dragIndex, 1);
+
+        const dropIndex = array.indexOf(dropWidgetId);
+        array.splice(dropIndex, 1);
+
+        if (dragIndex > dropIndex) {
+            array.splice(dropIndex, 0, dragWidgetId);
+            array.splice(dragIndex, 0, dropWidgetId);
+        } else {
+            array.splice(dragIndex, 0, dropWidgetId);
+            array.splice(dropIndex + 1, 0, dragWidgetId);
+        }
+
+        this.state.widgets = Array.from(array);
+
+        event.dataTransfer.dropEffect = 'move';
+    }
+
+    public dragEnter(event): void {
+        if (event.target.className === "widget-item") {
+            event.target.classList.add('over');
+        }
+    }
+
+    public dragLeave(event): void {
+        event.target.classList.remove('over');
+    }
+
+    public drop(event): void {
+        event.preventDefault();
+        this.state.tempWidgets = [];
+        this.resetCSS();
+    }
+
+    private resetCSS(): void {
+        for (const widgetId of this.state.widgets) {
+            const element = document.getElementById(widgetId);
+            element.classList.remove('over');
+            element.classList.remove('drag');
+        }
     }
 }
 
