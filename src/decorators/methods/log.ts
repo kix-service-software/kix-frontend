e1214016e1a8e8b91a0aac22a7fe74e8908f86ab
David Hein <david.hein@cape-it.de>
2017-09-01 09:18:31 +0200

    correct syntax for typescript 2.5

--- src/decorators/methods/log.ts
+++ src/decorators/methods/log.ts
@@ -1,7 +1,7 @@
 import { container } from '../../Container';
 import { ILoggingService } from '../../services/';
 
-function log(target: any, propertyName: string, descriptor: any): MethodDecorator {
+function log(target: any, propertyName: string, descriptor: TypedPropertyDescriptor<any>): any {
     const orgMethod = descriptor.value;
     // TODO: replace container.get with @inject within a constructor
     const loggingService = container.getDIContainer().get<ILoggingService>("ILoggingService");
