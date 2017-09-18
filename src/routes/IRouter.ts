332e8c7a491d2c2bfe956900e9004e25873354e2
David Hein <david.hein@cape-it.de>
2017-09-18 13:29:58 +0200

    test public var

--- src/routes/IRouter.ts
+++ src/routes/IRouter.ts
@@ -1,6 +1,20 @@
+import { IUserService } from './../services/IUserService';
+import { IPluginService } from './../services/IPluginService';
+import { IAuthenticationService } from './../services/IAuthenticationService';
+import { IConfigurationService } from './../services/IConfigurationService';
 import { Router } from 'express';
 
 export interface IRouter {
+
+    router: Router;
+
+    configurationService: IConfigurationService;
+
+    authenticationService: IAuthenticationService;
+
+    pluginService: IPluginService;
+
+    userService: IUserService;
 
     getRouter(): Router;
 
--- src/routes/KIXRouter.ts
+++ src/routes/KIXRouter.ts
@@ -7,12 +7,11 @@ import { Router, Response, Request } from 'express';
 
 @injectable()
 export abstract class KIXRouter implements IRouter {
-
-    protected router: Router;
-    protected configurationService: IConfigurationService;
-    protected authenticationService: IAuthenticationService;
-    protected pluginService: IPluginService;
-    protected userService: IUserService;
+    public router: Router;
+    public configurationService: IConfigurationService;
+    public authenticationService: IAuthenticationService;
+    public pluginService: IPluginService;
+    public userService: IUserService;
     protected serverConfig: IServerConfiguration;
 
     public constructor(
