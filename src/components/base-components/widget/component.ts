e90bdd4641c1226b9800aefdac329cf390ab634c
David Hein <david.hein@cape-it.de>
2017-10-13 15:16:24 +0200

    minimize widget

--- src/components/base-components/widget/component.ts
+++ src/components/base-components/widget/component.ts
@@ -5,8 +5,15 @@ class WidgetComponent {
     public state: any;
 
     public onCreate(input: any): void {
-        this.state = {};
+        this.state = {
+            minimized: true
+        };
     }
+
+    public minimizeWidget(): void {
+        this.state.minimized = !this.state.minimized;
+    }
+
 }
 
 module.exports = WidgetComponent;
