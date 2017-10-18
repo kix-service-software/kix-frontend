0819aa64731a037578265c79a675415a04d8df98
rkaiser <ricky.kaiser@cape-it.de>
2017-10-18 13:28:50 +0200

    added rudimentary editor component (CKEditor v4)

--- src/components/base-components/editor/component.ts
+++ src/components/base-components/editor/component.ts
@@ -1,40 +1,46 @@
-// declare var ckEditor: any;
+import { EditorComponentState } from './model/EditorComponentState';
+declare var CKEDITOR: any;
 
 class EditorComponent {
 
-    public state: any;
-    public ckEditor: any;
+    public state: EditorComponentState;
 
     public onCreate(input: any): void {
-        this.ckEditor = require('@ckeditor/ckeditor5-build-classic');
-
-        this.state = {
-            value: (input.defaultValue ? input.defaultValue : ''),
-            type: input.type,
-            inline: input.inline,
-            id: 'editor-' + Date.now(),
-        };
+        this.state = new EditorComponentState((input.inline ? true : false), input.resize, input.resizeDir);
+        this.state.value = (input.defaultValue ? input.defaultValue : '');
+        this.state.inline = (input.inline ? true : false);
     }
 
     public onMount(): void {
-        const type = 5;
-        if (type === 5) {
-            this.ckEditor
-                .create(document.querySelector('#' + this.state.id))
-                .then((editor) => {
-                    console.log(editor);
-                })
-                .catch((error) => {
-                    console.error(error);
-                });
+        if (this.state.inline) {
+            CKEDITOR.inline(this.state.id, {
+                ...this.state.config
+            });
         } else {
-            console.log('bla');
+            CKEDITOR.replace(this.state.id, {
+                ...this.state.config
+            });
         }
+        // TODO: maybe not necessary
+        (this as any).emit('editorInitialized', this.state.id);
     }
 
     public valueChanged(event: any): void {
         this.state.value = event.target.value;
         (this as any).emit('valueChanged', this.state.value);
+    }
+
+    /**
+     * Adds html text to the richtext editor on cursor position
+     *
+     * @param text the html text string
+     *
+     * @return nothing
+     */
+    public addHTML(text: string): void {
+        if (CKEDITOR.instances && CKEDITOR.instances[this.state.id]) {
+            CKEDITOR.instances[this.state.id].insertHtml(text);
+        }
     }
 }
 
