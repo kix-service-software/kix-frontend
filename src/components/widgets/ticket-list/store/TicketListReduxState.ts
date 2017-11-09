a0e05683472c3675212faa36b7aa69560c0be81b
Ricky Kaiser <ricky.kaiser@cape-it.de>
2017-11-09 10:32:07 +0100

    Update TicketListReduxState.ts - removed unnecessary attribute (users)

--- src/components/widgets/ticket-list/store/TicketListReduxState.ts
+++ src/components/widgets/ticket-list/store/TicketListReduxState.ts
@@ -4,8 +4,6 @@ import { TicketListSocketListener } from '../socket/TicketListSocketListener';
 
 export class TicketListReduxState extends WidgetReduxState {
 
-    public users: any[] = [];
-
     public configuration: any;
 
     public tickets: Ticket[];
