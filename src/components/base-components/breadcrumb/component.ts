cd4b19b4cb6ebde3973c1684266b0fe85ad7cd1f
David Hein <david.hein@cape-it.de>
2017-12-04 14:09:30 +0100

    #180 base breadcrumb function

--- src/components/base-components/breadcrumb/component.ts
+++ src/components/base-components/breadcrumb/component.ts
@@ -1,9 +1,23 @@
+import { KIXRouterStore } from "@kix/core/dist/browser/router/KIXRouterStore";
+
 class BreadcrumbComponent {
 
     public state: any;
 
     public onCreate(input: any): void {
-        this.state = {};
+        this.state = {
+            history: []
+        };
+    }
+
+    public onMount(): void {
+        KIXRouterStore.getInstance().addStateListener(this.routerStateChanged.bind(this));
+    }
+
+    private routerStateChanged(): void {
+        const history: Array<[string, any]> = KIXRouterStore.getInstance().getCurrentRouterHistory('base-router');
+        this.state.history = history ? history : [];
+        (this as any).setStateDirty('history');
     }
 
 }
