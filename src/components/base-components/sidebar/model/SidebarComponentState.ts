94e81b0f5fe37472fd644811c0703962bf5f319c
rkaiser <ricky.kaiser@cape-it.de>
2017-12-11 14:23:23 +0100

    removed unnecessary property

--- src/components/base-components/sidebar/model/SidebarComponentState.ts
+++ src/components/base-components/sidebar/model/SidebarComponentState.ts
@@ -3,7 +3,6 @@ import { ConfiguredWidget, WidgetTemplate } from '@kix/core/dist/model';
 export class SidebarComponentState {
 
     public configuredWidgets: ConfiguredWidget[] = [];
-    public configurationMode: boolean = false;
     public showIconBar: boolean = true;
     public rows: string[] = [];
     public context: string = "dashboard";
