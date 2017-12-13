5edcc62ce67e3be3a28c21b5ab70dcc3a19b0af1
David Hein <david.hein@cape-it.de>
2017-12-13 12:36:19 +0100

    #171 ticket subfolder

--- src/registry-modules/tickets/creation-dialog.extension.ts
+++ src/registry-modules/tickets/creation-dialog.extension.ts
@@ -15,7 +15,7 @@ export class TicketCreationDialogExtension implements ICreationDialogExtension {
     private getTemplatePath(): string {
         const packageJson = require('../../../package.json');
         const version = packageJson.version;
-        return '/@kix/frontend$' + version + '/dist/components/ticket-creation-dialog/';
+        return '/@kix/frontend$' + version + '/dist/components/ticket/ticket-creation-dialog/';
     }
 }
 
