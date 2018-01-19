3e96dc3800ff4035c7ad6395deab78ba5d2c9a6e
David Hein <david.hein@cape-it.de>
2018-01-19 13:38:44 +0100

    #253 Callback Resource

---
+++ src/services/api/ClientRegistrationService.ts
@@ -0,0 +1,43 @@
+import { ObjectService } from './ObjectService';
+import { IClientRegistrationService } from '@kix/core/dist/services';
+import { ClientRegistration } from '@kix/core/dist/model';
+import {
+    CreateClientRegistration,
+    CreateClientRegistrationResponse,
+    CreateClientRegistrationRequest,
+    ClientRegistrationsResponse,
+    ClientRegistrationResponse
+} from '@kix/core/dist/api';
+import { SortOrder } from '@kix/core/dist/browser/SortOrder';
+
+export class ClientRegistrationService extends ObjectService<ClientRegistration> implements IClientRegistrationService {
+
+    protected RESOURCE_URI: string = "clientregistration";
+
+    public async getClientRegistrations(
+        token: string, limit?: number, order?: SortOrder, changedAfter?: string, query?: any
+    ): Promise<ClientRegistration[]> {
+
+        const response = await this.getObjects<ClientRegistrationsResponse>(
+            token, limit, order, changedAfter, query
+        );
+
+        return response.ClientRegistration;
+    }
+
+    public async createClientRegistration(
+        token: string, createClientRegistration: CreateClientRegistration
+    ): Promise<string> {
+        const response = await this.createObject<CreateClientRegistrationResponse, CreateClientRegistrationRequest>(
+            token, this.RESOURCE_URI, new CreateClientRegistrationRequest(createClientRegistration)
+        );
+
+        return response.ClientID;
+    }
+
+    public async deleteClientRegistration(token: string, clientId: number): Promise<void> {
+        const uri = this.buildUri(this.RESOURCE_URI, clientId);
+        await this.deleteObject<void>(token, uri);
+    }
+
+}
