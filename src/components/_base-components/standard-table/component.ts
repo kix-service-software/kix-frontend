c8b7b89f37a3b80448ed7bedae8fe1a28eecd2d3
Ulrich Boehm <ulrich.boehm@cape-it.de>
2018-02-12 10:52:10 +0100

    304-Tabelle-wip

--- src/components/_base-components/standard-table/component.ts
+++ src/components/_base-components/standard-table/component.ts
@@ -1,3 +1,4 @@
+declare var PerfectScrollbar: any;
 import { StandardTableComponentState } from './StandardTableComponentState';
 import { StandardTableInput } from './StandardTableInput';
 import { StandardTableConfiguration, StandardTableColumn } from '@kix/core/dist/browser';
@@ -21,7 +22,45 @@ class StandardTableComponent<T> {
             this.state.tableConfiguration.contentProvider.addListener(() => {
                 (this as any).forceUpdate();
             });
+            const psY = new PerfectScrollbar('.standard-table', {
+                minScrollbarLength: 50,
+                wrapperElement: '.standard-table-wrapper'
+            });
         }
+
+        /*
+         * Code für fixed Header und fixed Column inkl horiz. und vertik. Scrollbar
+            var tableWrapper = document.getElementsByClassName('standard-table-wrapper');
+            var table        = document.getElementsByClassName('standard-table');
+            var header       = document.getElementsByClassName('header-row');
+            var fixedLeft    = document.getElementsByClassName('table-cell fixed fixed-left');
+            var fixedRight   = document.getElementsByClassName('table-cell fixed fixed-right');
+            var scrollbar    = document.getElementsByClassName('table-cell fixed fixed-right scrollbar-column');
+            var subRow       = document.getElementsByClassName('sub-row-wrapper visible');
+            // Header fixieren
+            table[0].addEventListener('ps-scroll-y', function() {
+                for (var x=0; x<header.length; x++) {
+                    header[x].style.top = table[0].scrollTop + 'px';
+                }
+            });
+            // linke, rechte Spalte und geöffnete Zeile fixieren
+            table[0].addEventListener('ps-scroll-x', function() {
+                for (var x=0; x<fixedLeft.length; x++) {
+                    fixedLeft[x].style.left = table[0].scrollLeft + 'px';
+                }
+                for (var x=0; x<fixedRight.length; x++) {
+                    fixedRight[x].style.right = ((table[0].scrollLeft + 24) * -1) + 'px';
+                }
+                for (var x=0; x<scrollbar.length; x++) {
+                    // 24 == 2em == Breite der Scrollbarspalte
+                    scrollbar[x].style.right = (table[0].scrollLeft * -1) + 'px';
+                }
+                for (var x=0; x<subRow.length; x++) {
+                    subRow[x].style.left = table[0].scrollLeft + 'px';
+                }
+            });
+        */
+
     }
 
     private getRows(): T[] {
