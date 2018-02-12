a87440fdf682621bc9bc90a9b80ef40340cf8450
rkaiser <ricky.kaiser@cape-it.de>
2018-02-12 15:34:50 +0100

    #306 added component state for ticket-property-label

--- src/components/ticket/ticket-labels/ticket-property-label/component.ts
+++ src/components/ticket/ticket-labels/ticket-property-label/component.ts
@@ -1,26 +1,26 @@
 import { TicketLabelComponentMapper } from './TicketLabelComponentMapper';
 import { TicketProperty } from '@kix/core/dist/model';
+import { TicketPropertyLabelComponentState } from './TicketPropertyLabelComponentState';
+
 export class TicketLabelComponent {
 
     private state: any;
 
     public onCreate(): void {
-        this.state = {
-            hasLabel: true,
-            hasText: true,
-            hasIcon: true,
-            ticket: null,
-            ticketId: null,
-            property: null,
-            value: null
-        };
+        this.state = new TicketPropertyLabelComponentState();
     }
 
     public onInput(input: any): void {
         this.state.ticket = input.ticket;
-        this.state.ticketId = this.state.ticket[TicketProperty.TICKET_ID];
         this.state.property = input.property;
-        this.state.value = this.state.ticket[this.state.property];
+
+        if (this.state.ticket) {
+            this.state.ticketId = this.state.ticket[TicketProperty.TICKET_ID];
+            if (this.state.property) {
+                this.state.value = this.state.ticket[this.state.property];
+            }
+        }
+
         this.state.hasLabel = typeof input.hasLabel !== 'undefined' ? input.hasLabel : true;
         this.state.hasText = typeof input.hasText !== 'undefined' ? input.hasText : true;
         this.state.hasIcon = typeof input.hasIcon !== 'undefined' ? input.hasIcon : true;
