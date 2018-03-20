d6d552bf90a0b16c324c9d98449e10fe6ac3b023
Dorothea Doerffel <dorothea.doerffel@cape-it.de>
2018-03-20 14:20:23 +0100

    #368 - added missing changes from merge

--- src/components/_base-components/widget/BaseWidgetComponentState.ts
+++ src/components/_base-components/widget/BaseWidgetComponentState.ts
@@ -6,7 +6,8 @@ export class BaseWidgetComponentState extends WidgetComponentState {
     public constructor(
         public configChanged: boolean = false,
         public hasConfigOverlay: boolean = true,
-        public isLoading: boolean = false
+        public isLoading: boolean = false,
+        public type: WidgetType = null
     ) {
         super();
     }
