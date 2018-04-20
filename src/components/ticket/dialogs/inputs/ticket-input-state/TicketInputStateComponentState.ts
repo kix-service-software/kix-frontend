b5494efb55ecce1d28488b3db474307e6f890dcd
David Hein <david.hein@cape-it.de>
2018-04-20 09:22:53 +0200

    #540 set state correctly

--- src/components/ticket/dialogs/inputs/ticket-input-state/TicketInputStateComponentState.ts
+++ src/components/ticket/dialogs/inputs/ticket-input-state/TicketInputStateComponentState.ts
@@ -4,6 +4,7 @@ export class TicketInputStateComponentState extends FormInputComponentState {
 
     public constructor(
         public items: FormDropdownItem[] = [],
+        public currentItem: FormDropdownItem = null,
         public pending: boolean = false,
         public times: string[] = [
             "00:00", "00:30", "01:00", "01:30", "02:00", "02:30", "03:00", "03:30", "04:00", "04:30",
