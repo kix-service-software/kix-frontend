f10d07958734f2e7a8ad11043fd8e7c448ca5ce6
David Hein <david.hein@cape-it.de>
2018-04-20 08:30:04 +0200

    #542 cleanup

--- src/components/_base-components/input-select-with-properties/model/SelectWithPropertiesComponentState.ts
+++
@@ -1,8 +0,0 @@
-import { SelectWithPropertiesListElement, SelectWithPropertiesProperty } from '@kix/core/dist/model';
-
-export class SelectWithPropertiesComponentState {
-    public constructor(
-        public list: SelectWithPropertiesListElement[],
-        public properties: SelectWithPropertiesProperty[]
-    ) { }
-}
