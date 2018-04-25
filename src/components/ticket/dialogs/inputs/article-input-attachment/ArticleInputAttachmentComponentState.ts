d7f8db21800bada302444c4fa1b847cc652ec0b3
David Hein <david.hein@cape-it.de>
2018-04-25 12:52:57 +0200

    #554 dnd files from fs to attachment component

--- src/components/ticket/dialogs/inputs/article-input-attachment/ArticleInputAttachmentComponentState.ts
+++ src/components/ticket/dialogs/inputs/article-input-attachment/ArticleInputAttachmentComponentState.ts
@@ -3,7 +3,8 @@ import { FormField, FormInputComponentState } from "@kix/core/dist/model";
 export class ArticleInputAttachmentComponentState extends FormInputComponentState {
 
     public constructor(
-        public files: File[] = []
+        public files: File[] = [],
+        public dragging: boolean = false
     ) {
         super();
     }
--- src/components/ticket/dialogs/inputs/article-input-attachment/component.ts
+++ src/components/ticket/dialogs/inputs/article-input-attachment/component.ts
@@ -14,6 +14,11 @@ class ArticleInputAttachmentComponent {
         this.state.formId = input.formId;
     }
 
+    public onMount(): void {
+        document.addEventListener("dragover", this.dragOver.bind(this), false);
+        document.addEventListener("drop", this.drop.bind(this), false);
+    }
+
     private triggerFileUpload(): void {
         const uploadInput = (this as any).getEl('fileUploadInput');
         if (uploadInput) {
@@ -24,14 +29,34 @@ class ArticleInputAttachmentComponent {
     private setAttachments(): void {
         const uploadInput = (this as any).getEl('fileUploadInput');
         if (uploadInput && uploadInput.files) {
-            const files = Array.from(uploadInput.files);
-            files.forEach((f: File) => {
-                if (this.state.files.findIndex((sf) => sf.name === f.name) === -1) {
-                    this.state.files.push(f);
-                }
-            });
-            (this as any).setStateDirty('files');
+            this.appendFiles(Array.from(uploadInput.files));
         }
+    }
+
+    private appendFiles(files: File[]): void {
+        files.forEach((f: File) => {
+            if (this.state.files.findIndex((sf) => sf.name === f.name) === -1) {
+                this.state.files.push(f);
+            }
+        });
+        (this as any).setStateDirty('files');
+    }
+
+    private dragOver(event: any): void {
+        event.stopPropagation();
+        event.preventDefault();
+        event.dataTransfer.dropEffect = 'copy';
+        this.state.dragging = true;
+    }
+
+    private drop(event: any): void {
+        event.stopPropagation();
+        event.preventDefault();
+
+        const files = event.dataTransfer.files;
+        this.appendFiles(Array.from(files));
+
+        this.state.dragging = false;
     }
 
 }
