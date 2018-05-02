65c11acd8d9978c2e1de8aa2bc1cd7cc93e9bf35
rkaiser <ricky.kaiser@cape-it.de>
2018-05-02 09:35:44 +0200

    #561 added dropdown for link object selection; some styling

--- src/components/ticket/dialogs/link-ticket-dialog/LinkTicketDialogComponentState.ts
+++ src/components/ticket/dialogs/link-ticket-dialog/LinkTicketDialogComponentState.ts
@@ -1,3 +1,9 @@
+import { FormDropdownItem } from "@kix/core/dist/model";
+
 export class LinkTicketDialogComponentState {
 
+    public constructor(
+        public linkableObjects: FormDropdownItem[] = [],
+        public currentItem: FormDropdownItem = null
+    ) { }
 }
--- src/components/ticket/dialogs/link-ticket-dialog/component.ts
+++ src/components/ticket/dialogs/link-ticket-dialog/component.ts
@@ -1,6 +1,7 @@
-import { ContextService, ContextNotification } from "@kix/core/dist/browser/context";
-import { Context, WidgetType } from "@kix/core/dist/model";
+import { ContextService } from "@kix/core/dist/browser/context";
 import { LinkTicketDialogComponentState } from './LinkTicketDialogComponentState';
+import { KIXObjectType, ObjectData, FormContext, FormDropdownItem } from "@kix/core/dist/model";
+import { FormService } from "@kix/core/dist/browser/form";
 
 class LinkTicketDialogComponent {
 
@@ -10,6 +11,44 @@ class LinkTicketDialogComponent {
         this.state = new LinkTicketDialogComponentState();
     }
 
+    public onMount(): void {
+        const objectData = ContextService.getInstance().getObjectData();
+        if (objectData && objectData.linkTypes) {
+            this.setLinkableObjects(objectData);
+            if (this.state.linkableObjects.length) {
+                const linkableTicket = this.state.linkableObjects.find((lo) => lo.label === KIXObjectType.TICKET);
+                if (linkableTicket) {
+                    this.state.currentItem = linkableTicket;
+                } else {
+                    this.state.currentItem = this.state.linkableObjects[0];
+                }
+            }
+        }
+    }
+
+    public setLinkableObjects(objectData: ObjectData): void {
+        objectData.linkTypes.forEach((lt) => {
+            let linkableObject = null;
+            if (lt.Source === KIXObjectType.TICKET) {
+                linkableObject = lt.Target;
+            } else if (lt.Target === KIXObjectType.TICKET) {
+                linkableObject = lt.Source;
+            }
+            if (linkableObject && !this.state.linkableObjects.some((lo) => lo.label === linkableObject)) {
+                const formId = FormService.getInstance().getFormIdByContext(FormContext.LINK, linkableObject);
+                if (formId) {
+                    this.state.linkableObjects.push(new FormDropdownItem(formId, '', linkableObject));
+                }
+            }
+        });
+        if (this.state.linkableObjects.length) {
+            (this as any).setStateDirty('linkableObjects');
+        }
+    }
+
+    private itemChanged(item: FormDropdownItem): void {
+        this.state.currentItem = item;
+    }
 }
 
 module.exports = LinkTicketDialogComponent;
