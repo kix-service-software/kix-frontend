c62d019939e5a0069e57098874c13b8e93616bc5
David Hein <david.hein@cape-it.de>
2018-06-04 12:00:15 +0200

    open tickets count

--- src/communicators/ContactCommunicator.ts
+++ src/communicators/ContactCommunicator.ts
@@ -14,7 +14,6 @@ export class ContactCommunicator extends KIXCommunicator {
     }
 
     protected registerEvents(): void {
-        this.registerEventHandler(ContactEvent.GET_TICKET_COUNT, this.getTicketCount.bind(this));
         this.registerEventHandler(ContactEvent.LOAD_CONTACTS, this.loadContacts.bind(this));
         this.registerEventHandler(ContactEvent.CREATE_CONTACT, this.createContact.bind(this));
     }
@@ -30,21 +29,6 @@ export class ContactCommunicator extends KIXCommunicator {
         }
         const response = new ContactsLoadResponse(data.requestId, contacts);
         return new CommunicatorResponse(ContactEvent.CONTACTS_LOADED, response);
-    }
-
-    private async getTicketCount(
-        data: GetContactTicketCountRequest
-    ): Promise<CommunicatorResponse<GetContactTicketCountResponse>> {
-        let response;
-        await this.ticketService.getTicketCountForContact(
-            data.token, data.contactId, data.stateTypeIds
-        ).then((ticketCount: number) => {
-            response = new GetContactTicketCountResponse(data.requestId, ticketCount);
-        }).catch((error) => {
-            return new CommunicatorResponse(ContactEvent.GET_TICKET_COUNT_ERROR, error);
-        });
-
-        return new CommunicatorResponse(ContactEvent.GET_TICKET_COUNT_FINISHED, response);
     }
 
     private async createContact(data: CreateContactRequest): Promise<CommunicatorResponse<CreateContactResponse>> {
