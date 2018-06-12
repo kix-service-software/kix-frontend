2d55e06f022987396a1c897f325082ddbebc23e6
Dorothea Doerffel <dorothea.doerffel@cape-it.de>
2018-06-12 11:19:32 +0200

    #742 - home-context registration

---
+++ src/registry-modules/home/home-marko.extensions.ts
@@ -0,0 +1,25 @@
+import { IMarkoDependencyExtension } from '@kix/core/dist/extensions';
+
+export class HomeMarkoDependencyExtension implements IMarkoDependencyExtension {
+
+    public getDependencies(): string[] {
+        return [
+            'home/home-module',
+        ];
+    }
+
+    public getComponentTags(): Array<[string, string]> {
+        return [
+            ['home', 'home/home-module'],
+        ];
+    }
+
+    public isExternal(): boolean {
+        return false;
+    }
+
+}
+
+module.exports = (data, host, options) => {
+    return new HomeMarkoDependencyExtension();
+};
