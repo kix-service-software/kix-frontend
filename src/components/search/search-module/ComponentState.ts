eb425427593b1c75290f090d1169e55307e59152
Dorothea Doerffel <dorothea.doerffel@cape-it.de>
2018-06-25 14:58:39 +0200

    #608 - added search criteria widget

--- src/components/search/search-module/ComponentState.ts
+++ src/components/search/search-module/ComponentState.ts
@@ -1,3 +1,20 @@
+import { FilterCriteria, FilterDataType, FilterType } from '@kix/core/dist/model';
+import { SearchOperator } from '@kix/core/dist/browser';
 export class ComponentState {
-
+    public constructor(
+        public criterias: FilterCriteria[] = [
+            new FilterCriteria('Property 1', SearchOperator.EQUALS, FilterDataType.STRING, FilterType.AND, 'test'),
+            new FilterCriteria('Property 2', SearchOperator.EQUALS, FilterDataType.STRING, FilterType.AND, 'test'),
+            new FilterCriteria('Property 3', SearchOperator.EQUALS, FilterDataType.STRING, FilterType.AND, 'test'),
+            new FilterCriteria('Property 4', SearchOperator.EQUALS, FilterDataType.STRING, FilterType.AND, 'test'),
+            new FilterCriteria('Property 5', SearchOperator.EQUALS, FilterDataType.STRING, FilterType.AND, 'test'),
+            new FilterCriteria('Property 6', SearchOperator.EQUALS, FilterDataType.STRING, FilterType.AND, 'test'),
+            new FilterCriteria('Property 7', SearchOperator.EQUALS, FilterDataType.STRING, FilterType.AND, 'test'),
+            new FilterCriteria('Property 8', SearchOperator.EQUALS, FilterDataType.STRING, FilterType.AND, 'test'),
+            new FilterCriteria('Property 9', SearchOperator.EQUALS, FilterDataType.STRING, FilterType.AND, 'test'),
+            new FilterCriteria('Property 10', SearchOperator.EQUALS, FilterDataType.STRING, FilterType.AND, 'test'),
+            new FilterCriteria('Property 11', SearchOperator.EQUALS, FilterDataType.STRING, FilterType.AND, 'test'),
+            new FilterCriteria('Property 12', SearchOperator.EQUALS, FilterDataType.STRING, FilterType.AND, 'test')
+        ]
+    ) { }
 }
--- src/components/search/search-module/component.ts
+++ src/components/search/search-module/component.ts
@@ -1,11 +1,20 @@
+import { ComponentState } from './ComponentState';
 import { ContextService } from "@kix/core/dist/browser";
 import { ContextMode } from "@kix/core/dist/model";
 
 class Component {
 
+    private state: ComponentState;
+
+    public onCreate(): void {
+        this.state = new ComponentState();
+
+    }
+
     public onMount(): void {
         ContextService.getInstance().setDialogContext(null, null, ContextMode.SEARCH);
     }
+
 
 }
 
