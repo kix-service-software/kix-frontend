ef6abc6c27913733d9341b633a1a5f57b27bd8a1
David Hein <david.hein@cape-it.de>
2018-06-26 11:27:11 +0200

    customer search

---
+++ src/components/customer/dialogs/search-customer-dialog/component.ts
@@ -0,0 +1,25 @@
+import { ComponentState } from "./ComponentState";
+import { FormService, DialogService } from "@kix/core/dist/browser";
+import { FormContext, KIXObjectType } from "@kix/core/dist/model";
+
+class Component {
+
+    private state: ComponentState;
+
+    public onCreate(): void {
+        this.state = new ComponentState();
+    }
+
+    public onMount(): void {
+        this.state.formId = FormService.getInstance().getFormIdByContext(FormContext.SEARCH, KIXObjectType.CUSTOMER);
+    }
+
+    public cancel(): void {
+        const formInstance = FormService.getInstance().getFormInstance(this.state.formId);
+        formInstance.reset();
+        DialogService.getInstance().closeMainDialog();
+    }
+
+}
+
+module.exports = Component;
--- src/registry-modules/customer/customer-marko.extension.ts
+++ src/registry-modules/customer/customer-marko.extension.ts
@@ -18,6 +18,7 @@ export class CustomerMarkoDependencyExtension implements IMarkoDependencyExtensi
             'customer/dialogs/new-customer-dialog',
             'customer/dialogs/new-contact-dialog',
             'customer/dialogs/search-contact-dialog',
+            'customer/dialogs/search-customer-dialog',
             'customer/dialogs/inputs/customer-input-valid',
             'customer/dialogs/inputs/contact-input-customer'
         ];
@@ -43,6 +44,7 @@ export class CustomerMarkoDependencyExtension implements IMarkoDependencyExtensi
             ['new-customer-dialog', 'customer/dialogs/new-customer-dialog'],
             ['new-contact-dialog', 'customer/dialogs/new-contact-dialog'],
             ['search-contact-dialog', 'customer/dialogs/search-contact-dialog'],
+            ['search-customer-dialog', 'customer/dialogs/search-customer-dialog'],
             ['customer-input-valid', 'customer/dialogs/inputs/customer-input-valid'],
             ['contact-input-customer', 'customer/dialogs/inputs/contact-input-customer']
         ];
