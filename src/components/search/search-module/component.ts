9862a543ddd1d8b313976a1d76eb5d3f1f5e3741
David Hein <david.hein@cape-it.de>
2018-06-26 14:25:30 +0200

    search context history

--- src/components/search/search-module/component.ts
+++ src/components/search/search-module/component.ts
@@ -1,5 +1,5 @@
 import { ComponentState } from './ComponentState';
-import { ContextService, KIXObjectSearchCache, KIXObjectSearchService } from "@kix/core/dist/browser";
+import { ContextService, KIXObjectSearchService } from "@kix/core/dist/browser";
 import { ContextMode } from "@kix/core/dist/model";
 
 class Component {
@@ -8,18 +8,22 @@ class Component {
 
     public onCreate(): void {
         this.state = new ComponentState();
+    }
 
+    public onInput(input: any): void {
+        this.state.fromHistory = input.fromHistory;
     }
 
     public onMount(): void {
-        ContextService.getInstance().setDialogContext(null, null, ContextMode.SEARCH);
+        if (!this.state.fromHistory) {
+            ContextService.getInstance().setDialogContext(null, null, ContextMode.SEARCH);
+        }
     }
 
     public openSearchDialog(): void {
         KIXObjectSearchService.getInstance().clearSearchCache();
         ContextService.getInstance().setDialogContext(null, null, ContextMode.SEARCH);
     }
-
 
 }
 
