dd58cad8d9d358d37bb3b3589e88ea3662eef5b5
David Hein <david.hein@cape-it.de>
2018-06-27 15:52:44 +0200

    search result widget

--- src/components/search/search-module/ComponentState.ts
+++ src/components/search/search-module/ComponentState.ts
@@ -1,6 +1,13 @@
+import { ObjectIcon, KIXObject } from "@kix/core/dist/model";
+import { StandardTable } from "@kix/core/dist/browser";
+
 export class ComponentState {
     public constructor(
         public criterias: Array<[string, string, string]> = [],
-        public fromHistory: boolean = false
+        public fromHistory: boolean = false,
+        public resultTitle: string = "Trefferliste:",
+        public resultIcon: string | ObjectIcon = null,
+        public resultTable: StandardTable<KIXObject> = null,
+        public noSearch: boolean = true
     ) { }
 }
