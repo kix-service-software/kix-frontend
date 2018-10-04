e6a039edf813aa4531478e908658602a21930511
David Hein <david.hein@cape-it.de>
2018-10-04 14:49:39 +0200

    fix interface declaration

--- src/components/_base-components/action-list/ComponentState.ts
+++ src/components/_base-components/action-list/ComponentState.ts
@@ -1,11 +1,11 @@
-import { ConfiguredWidget, Ticket, Article, AbstractAction } from '@kix/core/dist/model';
+import { IAction } from '@kix/core/dist/model';
 
 export class ComponentState {
 
     public constructor(
-        public actionList: AbstractAction[] = [],
-        public listDefault: AbstractAction[] = [],
-        public listExpansion: AbstractAction[] = [],
+        public actionList: IAction[] = [],
+        public listDefault: IAction[] = [],
+        public listExpansion: IAction[] = [],
         public showListExpansion: boolean = false,
         public keepShow: boolean = false
     ) { }
