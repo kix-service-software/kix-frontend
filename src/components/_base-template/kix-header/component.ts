e8367fc277f762e3204f1b27e388dbc7c49fdb32
Ricky Kaiser <ricky.kaiser@cape-it.de>
2018-10-17 14:01:35 +0200

    Us 1244 - added release modul

--- src/components/_base-template/kix-header/component.ts
+++ src/components/_base-template/kix-header/component.ts
@@ -1,6 +1,8 @@
 import { ClientStorageService } from "@kix/core/dist/browser/ClientStorageService";
 import { ContextService, OverlayService } from "@kix/core/dist/browser";
 import { ContextMode, ComponentContent, ToastContent, OverlayType } from "@kix/core/dist/model";
+import { RoutingConfiguration } from "@kix/core/dist/browser/router";
+import { ReleaseContext } from "@kix/core/dist/browser/release";
 
 class KIXHeaderComponent {
 
@@ -13,6 +15,12 @@ class KIXHeaderComponent {
             'Coming Soon', 'kix-icon-magicwand', 'Diese Funktionalität ist in Arbeit.'
         ));
         OverlayService.getInstance().openOverlay(OverlayType.HINT_TOAST, null, content, '');
+    }
+
+    public getReleaseRoutingConfig(): RoutingConfiguration {
+        return new RoutingConfiguration(
+            null, ReleaseContext.CONTEXT_ID, null, ContextMode.DASHBOARD, null
+        );
     }
 
     public logout(): void {
