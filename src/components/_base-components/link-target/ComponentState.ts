9548cf956f0ebc6d5e85196e4bf43fd44d176753
David Hein <david.hein@cape-it.de>
2018-11-01 15:21:45 +0100

    US-1381: link-target can handle object

--- src/components/_base-components/link-target/ComponentState.ts
+++ src/components/_base-components/link-target/ComponentState.ts
@@ -1,4 +1,4 @@
-import { ContextMode, KIXObjectType } from "@kix/core/dist/model";
+import { ContextMode, KIXObjectType, KIXObject } from "@kix/core/dist/model";
 import { RoutingConfiguration } from "@kix/core/dist/browser/router";
 
 export class ComponentState {
@@ -6,6 +6,7 @@ export class ComponentState {
     public constructor(
         public routingConfiguration: RoutingConfiguration = null,
         public objectId: string | number = null,
+        public object: KIXObject = null,
         public url: string = null,
         public loading: boolean = false
     ) { }
--- src/components/_base-components/link-target/component.ts
+++ src/components/_base-components/link-target/component.ts
@@ -1,6 +1,6 @@
 import { ComponentState } from './ComponentState';
 import { RoutingService } from '@kix/core/dist/browser/router';
-import { ContextService } from '@kix/core/dist/browser';
+import { ContextService, KIXObjectService } from '@kix/core/dist/browser';
 import { ContextType } from '@kix/core/dist/model';
 
 class Component {
@@ -14,6 +14,7 @@ class Component {
     public onInput(input: ComponentState): void {
         this.state.routingConfiguration = input.routingConfiguration ? { ...input.routingConfiguration } : null;
         this.state.objectId = input.objectId;
+        this.state.object = input.object;
         this.setURL();
     }
 
@@ -25,12 +26,15 @@ class Component {
             );
             this.state.routingConfiguration.path = contextUrl;
             this.state.url = '/' + contextUrl;
+        } else if (this.state.object) {
+            const url = await KIXObjectService.getObjectUrl(this.state.object);
+            this.state.url = '/' + url;
         }
         this.state.loading = false;
     }
 
     public linkClicked(event: any): void {
-        let externalLink = this.state.routingConfiguration.externalLink;
+        let externalLink = this.state.routingConfiguration ? this.state.routingConfiguration.externalLink : undefined;
         if (typeof externalLink === 'undefined') {
             const context = ContextService.getInstance().getActiveContext();
             externalLink = context ? context.getDescriptor().contextType === ContextType.DIALOG : false;
