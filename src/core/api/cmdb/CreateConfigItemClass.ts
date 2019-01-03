94f300674378629e8bc6d34a7a36bafc72644774
David Hein <david.hein@cape-it.de>
2019-01-03 15:03:52 +0100

    US-1510: create ci class with definition string

--- src/core/api/cmdb/CreateConfigItemClass.ts
+++ src/core/api/cmdb/CreateConfigItemClass.ts
@@ -1,10 +1,20 @@
 import { RequestObject } from "../RequestObject";
+import { ConfigItemClassProperty } from "../../model";
 
 export class CreateConfigItemClass extends RequestObject {
 
     public constructor(parameter: Array<[string, any]>) {
         super();
-        parameter.forEach((p) => this.applyProperty(p[0], p[1]));
+        parameter.forEach((p) => {
+            if (p[0] === ConfigItemClassProperty.DEFINITION_STRING) {
+                let definitionString = p[1].toString();
+                definitionString = definitionString
+                    .replace(/\n/g, "\n")
+                    .replace(/\u200B/g, "");
+                p[1] = definitionString;
+            }
+            this.applyProperty(p[0], p[1]);
+        });
     }
 
 }
