924965e3db964e9623c3f2d7ccfb1d8a7e05e3c3
dhein <david.hein@cape-it.de>
2019-04-09 08:45:39 +0200

    correct object type

--- src/core/api/role/create/CreateRole.ts
+++ src/core/api/role/create/CreateRole.ts
@@ -6,7 +6,7 @@ export class CreateRole extends RequestObject {
     public constructor(parameter: Array<[string, any]>) {
         super();
         parameter.forEach((p) => {
-            if (p[0] === RoleProperty.CONFIGURED_PERMISSIONS) {
+            if (p[0] === RoleProperty.PERMISSIONS) {
                 p[1].forEach((pd: CreatePermissionDescription) => {
                     delete (pd.ID);
                     delete (pd.RoleID);
--- src/core/browser/user/admin/RoleService.ts
+++ src/core/browser/user/admin/RoleService.ts
@@ -23,7 +23,7 @@ export class RoleService extends KIXObjectService<Role> {
     protected async prepareCreateValue(property: string, value: any): Promise<Array<[string, any]>> {
         const parameter: Array<[string, any]> = [];
         if (value) {
-            if (property === RoleProperty.USER_IDS || property === RoleProperty.CONFIGURED_PERMISSIONS) {
+            if (property === RoleProperty.USER_IDS || property === RoleProperty.PERMISSIONS) {
                 if (Array.isArray(value) && !!value.length) {
                     parameter.push([property, value]);
                 }
