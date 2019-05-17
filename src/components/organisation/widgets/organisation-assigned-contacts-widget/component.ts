c566de410d95fcfc6ab22ee45c3888a3f1a12fbf
Ricky Kaiser <ricky.kaiser@cape-it.de>
2019-05-17 11:16:16 +0200

    KIX2018-2120: show count in organisation assigned contacts widget

--- src/components/organisation/widgets/organisation-assigned-contacts-widget/component.ts
+++ src/components/organisation/widgets/organisation-assigned-contacts-widget/component.ts
@@ -43,6 +43,9 @@ class Component {
 
     private async initWidget(organisation: Organisation): Promise<void> {
         this.state.organisation = organisation;
+        this.state.title = this.state.widgetConfiguration.title
+            + (this.state.organisation.Contacts && !!this.state.organisation.Contacts.length ?
+                ` (${this.state.organisation.Contacts.length})` : '');
         this.prepareTable();
         this.prepareActions();
     }
