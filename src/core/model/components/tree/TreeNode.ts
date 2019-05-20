e1a0b5abb3a99b74a4acb6c50c4be56b207c3266
David Hein <david.hein@cape-it.de>
2019-05-20 09:38:51 +0200

    KIX2018-2113: filter tree based on flags

--- src/core/model/components/tree/TreeNode.ts
+++ src/core/model/components/tree/TreeNode.ts
@@ -16,6 +16,7 @@ export class TreeNode {
         public visible: boolean = false,
         public expandOnClick: boolean = false,
         public clickable: boolean = true,
-        public tooltip: string = label
+        public tooltip: string = label,
+        public flags: string[] = []
     ) { }
 }
