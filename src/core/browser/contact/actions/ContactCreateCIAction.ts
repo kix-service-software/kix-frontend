1679ac8f34b5142b4a8947b1efb6ab77061e0048
David Hein <david.hein@cape-it.de>
2019-06-13 09:51:39 +0200

    KIX2018-2202 implement action logik for create config item

--- src/core/browser/contact/actions/ContactCreateCIAction.ts
+++ src/core/browser/contact/actions/ContactCreateCIAction.ts
@@ -1,10 +1,19 @@
 import { AbstractAction } from '../../../model/components/action/AbstractAction';
+import { ContextService } from '../../context';
+import { ContextMode, KIXObjectType } from '../../../model';
 
 export class ContactCreateCIAction extends AbstractAction {
 
     public async initAction(): Promise<void> {
         this.text = 'Translatable#New Config Item';
         this.icon = 'kix-icon-cmdb';
+    }
+
+    public async run(event: any): Promise<void> {
+        ContextService.getInstance().setDialogContext(
+            null, KIXObjectType.CONFIG_ITEM, ContextMode.CREATE, null, true,
+            undefined, undefined, 'new-config-item-form'
+        );
     }
 
 }
