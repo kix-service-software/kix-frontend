7c80a9d1e5a7e5644b21ed902a5dee10c9c0cd85
dhein <david.hein@cape-it.de>
2019-06-13 10:34:47 +0200

    KIX2018-2080 KIX2018-1810 KIX2018-2235 add permissions for ticket bulk manager

--- src/components/ticket/module/ticket-update-module-component/component.ts
+++ src/components/ticket/module/ticket-update-module-component/component.ts
@@ -9,9 +9,11 @@ import {
 import { BulkService } from '../../../../core/browser/bulk';
 import {
     ContextDescriptor, KIXObjectType, ContextType, ContextMode,
-    ConfiguredDialogWidget, WidgetConfiguration, WidgetSize
+    ConfiguredDialogWidget, WidgetConfiguration, WidgetSize, CRUD
 } from '../../../../core/model';
 import { FormValidationService } from '../../../../core/browser/form/validation';
+import { AuthenticationSocketClient } from '../../../../core/browser/application/AuthenticationSocketClient';
+import { UIComponentPermission } from '../../../../core/model/UIComponentPermission';
 
 class Component extends AbstractMarkoComponent {
 
@@ -27,11 +29,19 @@ class Component extends AbstractMarkoComponent {
 
         TicketFormService.getInstance();
 
-        BulkService.getInstance().registerBulkManager(new TicketBulkManager());
+        if (await this.checkPermissions('tickets/*', [CRUD.UPDATE])) {
+            BulkService.getInstance().registerBulkManager(new TicketBulkManager());
+        }
 
         this.registerContexts();
         this.registerTicketActions();
         this.registerTicketDialogs();
+    }
+
+    private async checkPermissions(resource: string, crud: CRUD[]): Promise<boolean> {
+        return await AuthenticationSocketClient.getInstance().checkPermissions(
+            [new UIComponentPermission(resource, crud)]
+        );
     }
 
     private registerContexts(): void {
