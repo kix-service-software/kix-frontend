e335e2be24ef877d34b84b66985abbe55495ebba
David Hein <david.hein@cape-it.de>
2019-07-04 09:01:29 +0200

    KIX2018-2364 define action permissions

--- src/core/browser/contact/actions/ContactCreateOrganisationAction.ts
+++ src/core/browser/contact/actions/ContactCreateOrganisationAction.ts
@@ -1,7 +1,13 @@
 import { AbstractAction } from '../../../model/components/action/AbstractAction';
 import { OrganisationDialogUtil } from '../../organisation';
+import { UIComponentPermission } from '../../../model/UIComponentPermission';
+import { CRUD } from '../../../model';
 
 export class ContactCreateOrganisationAction extends AbstractAction {
+
+    public permissions = [
+        new UIComponentPermission('organisations', [CRUD.CREATE])
+    ];
 
     public async initAction(): Promise<void> {
         this.text = 'Translatable#New Organisation';
