6743270c2a30182ea87c28bd22b34b44056577b7
David Hein <david.hein@cape-it.de>
2019-07-18 11:16:24 +0200

    KIX2018-2085 license header for sources

--- src/components/_base-template/kix-header/component.ts
+++ src/components/_base-template/kix-header/component.ts
@@ -1,3 +1,12 @@
+/**
+ * Copyright (C) 2006-2019 c.a.p.e. IT GmbH, https://www.cape-it.de
+ * --
+ * This software comes with ABSOLUTELY NO WARRANTY. For details, see
+ * the enclosed file LICENSE for license information (GPL3). If you
+ * did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
+ * --
+ */
+
 import { ClientStorageService } from '../../../core/browser/ClientStorageService';
 import { ContextService, OverlayService, DialogService } from '../../../core/browser';
 import { ContextMode, ComponentContent, ToastContent, OverlayType, KIXObjectType } from '../../../core/model';
--- src/components/faq/widgets/faq-article-content-widget/component.ts
+++ src/components/faq/widgets/faq-article-content-widget/component.ts
@@ -1,3 +1,12 @@
+/**
+ * Copyright (C) 2006-2019 c.a.p.e. IT GmbH, https://www.cape-it.de
+ * --
+ * This software comes with ABSOLUTELY NO WARRANTY. For details, see
+ * the enclosed file LICENSE for license information (GPL3). If you
+ * did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
+ * --
+ */
+
 import { KIXObjectType, WidgetType, KIXObjectLoadingOptions, ObjectIcon, Context } from "../../../../core/model";
 import {
     ContextService, ActionFactory, WidgetService, BrowserUtil, IdService, KIXObjectService, LabelService
