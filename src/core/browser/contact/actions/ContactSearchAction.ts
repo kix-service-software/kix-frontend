f3ec9dbb81cc79e32554784faf21a0d58260d889
David Hein <david.hein@cape-it.de>
2019-07-24 12:28:32 +0200

    KIX2018-2463 implemented search action fro organisation and contacts

--- src/core/browser/contact/actions/ContactSearchAction.ts
+++ src/core/browser/contact/actions/ContactSearchAction.ts
@@ -8,12 +8,18 @@
  */
 
 import { AbstractAction } from '../../../model/components/action/AbstractAction';
+import { ContextService } from '../../context';
+import { KIXObjectType, ContextMode } from '../../../model';
 
 export class ContactSearchAction extends AbstractAction {
 
     public async initAction(): Promise<void> {
         this.text = 'Translatable#Search';
         this.icon = 'kix-icon-search';
+    }
+
+    public async run(event: any): Promise<void> {
+        ContextService.getInstance().setDialogContext(null, KIXObjectType.CONTACT, ContextMode.SEARCH, null, true);
     }
 
 }
