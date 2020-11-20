b5264c984f05df9462d2ae80b9634ed93a71ef70
Ricky Kaiser <ricky.kaiser@cape-it.de>
2020-11-20 08:02:06 +0100

    KIX2018-4700: show multiline if necessary

---
+++ src/frontend-applications/agent-portal/model/LabelValueGroupValue.ts
@@ -0,0 +1,20 @@
+/**
+ * Copyright (C) 2006-2020 c.a.p.e. IT GmbH, https://www.cape-it.de
+ * --
+ * This software comes with ABSOLUTELY NO WARRANTY. For details, see
+ * the enclosed file LICENSE for license information (GPL3). If you
+ * did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
+ * --
+ */
+
+import { Attachment } from './kix/Attachment';
+
+export class LabelValueGroupValue {
+
+    public constructor(
+        public value: string,
+        public multiline: boolean = false,
+        public attachment?: Attachment,
+    ) { }
+
+}
