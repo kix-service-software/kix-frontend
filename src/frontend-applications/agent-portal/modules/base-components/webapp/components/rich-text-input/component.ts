9daaee4849606f63857307f568442ae7ca56c642
David Hein <david.hein@cape-it.de>
2021-02-03 08:28:07 +0100

    KIX2018-4998 fixed wrong instanceId

--- src/frontend-applications/agent-portal/modules/base-components/webapp/components/rich-text-input/component.ts
+++ src/frontend-applications/agent-portal/modules/base-components/webapp/components/rich-text-input/component.ts
@@ -36,7 +36,7 @@ class Component extends FormInputComponent<string, ComponentState> {
         const autofillOption = this.state.field.options.find((o) => o.option === FormFieldOptions.AUTO_COMPLETE);
         if (autofillOption) {
             const autocompleteOption = (autofillOption.value as AutocompleteFormFieldOption);
-            const component = (this as any).getComponent(this.state.field.instanceId);
+            const component = (this as any).getComponent(this.state.field.instanceId + '-editor');
             if (component) {
                 component.setAutocompleteConfiguration(autocompleteOption);
             }
