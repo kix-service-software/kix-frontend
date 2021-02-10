caa72849f1dc90d92d83a0395a5cdda5f953aaa0
David Hein <david.hein@cape-it.de>
2021-02-10 10:32:57 +0100

    KIX2018-5053 disable buttons on submit

--- src/frontend-applications/agent-portal/modules/ticket/webapp/components/new-ticket-dialog/component.ts
+++ src/frontend-applications/agent-portal/modules/ticket/webapp/components/new-ticket-dialog/component.ts
@@ -93,16 +93,9 @@ class Component extends AbstractNewDialog {
     }
 
     public async submit(): Promise<void> {
-        await super.submit().then(async () => {
-            // const context = await ContextService.getInstance().getContext(TicketDetailsContext.CONTEXT_ID);
-            // const ticket = await context.getObject<Ticket>(KIXObjectType.TICKET, true, [TicketProperty.ARTICLES]);
-            // if (ticket) {
-            //     const article = [...ticket.Articles].sort((a, b) => b.ArticleID - a.ArticleID)[0];
-            //     if (article.isUnsent()) {
-            //         BrowserUtil.openErrorOverlay(article.getUnsentError());
-            //     }
-            // }
-        });
+        this.state.submitted = true;
+        await super.submit().catch((e) => null);
+        this.state.submitted = false;
     }
 
 }
