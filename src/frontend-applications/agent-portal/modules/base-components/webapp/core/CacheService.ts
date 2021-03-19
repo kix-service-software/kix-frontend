3de597c668178c36a909f7dac80f3673234d7123
Ricky Kaiser <ricky.kaiser@cape-it.de>
2021-03-19 06:46:09 +0000

    KIX2018-5231: delete "permission check" cache on ticket update

--- src/frontend-applications/agent-portal/modules/base-components/webapp/core/CacheService.ts
+++ src/frontend-applications/agent-portal/modules/base-components/webapp/core/CacheService.ts
@@ -143,6 +143,9 @@ export class BrowserCacheService {
                 cacheKeyPrefixes.push(KIXObjectType.QUEUE);
                 cacheKeyPrefixes.push(KIXObjectType.CURRENT_USER);
                 cacheKeyPrefixes.push(KIXObjectType.ARTICLE);
+                // needed for permission checks of objectactions (UIObjectActions/AuthenticationSocketClient)
+                // - check new after ticket update
+                cacheKeyPrefixes.push(KIXObjectType.ROLE);
                 break;
             case KIXObjectType.FAQ_VOTE:
                 cacheKeyPrefixes.push(KIXObjectType.FAQ_ARTICLE);
