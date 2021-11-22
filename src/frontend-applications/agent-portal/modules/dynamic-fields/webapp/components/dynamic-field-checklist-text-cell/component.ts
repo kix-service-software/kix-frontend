9587aba077b6a805ea1690d07362fdc77ccf6422
David Hein <david.hein@cape-it.de>
2021-11-22 06:14:12 +0000

    KIX2018-6009 textarea checklist item

---
+++ src/frontend-applications/agent-portal/modules/dynamic-fields/webapp/components/dynamic-field-checklist-text-cell/component.ts
@@ -0,0 +1,35 @@
+/**
+ * Copyright (C) 2006-2021 c.a.p.e. IT GmbH, https://www.cape-it.de
+ * --
+ * This software comes with ABSOLUTELY NO WARRANTY. For details, see
+ * the enclosed file LICENSE for license information (GPL3). If you
+ * did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
+ * --
+ */
+
+import { ComponentState } from './ComponentState';
+import { AbstractMarkoComponent } from '../../../../base-components/webapp/core/AbstractMarkoComponent';
+import { Cell } from '../../../../base-components/webapp/core/table';
+import { CheckListItem } from '../../core/CheckListItem';
+
+class Component extends AbstractMarkoComponent<ComponentState> {
+
+    public onCreate(): void {
+        this.state = new ComponentState();
+    }
+
+    public onInput(input: any): void {
+        const cell: Cell = input.cell;
+        if (cell) {
+            this.update(cell);
+        }
+    }
+
+    private async update(cell: Cell): Promise<void> {
+        const checklistItem = cell.getRow().getRowObject<CheckListItem>().getObject();
+        this.state.checklistItem = checklistItem;
+    }
+
+}
+
+module.exports = Component;
