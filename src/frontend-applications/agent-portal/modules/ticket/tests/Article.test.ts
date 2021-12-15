506bd9febb6ddda370cb9325514a43ac889ee0ae
David Hein <david.hein@cape-it.de>
2021-12-15 07:48:49 +0000

    KIX2018-6550 also search for file-1 and file-1.html

---
+++ src/frontend-applications/agent-portal/modules/ticket/tests/Article.test.ts
@@ -0,0 +1,74 @@
+// tslint:disable
+import chai = require('chai');
+import chaiAsPromised = require('chai-as-promised');
+import { Attachment } from '../../../model/kix/Attachment';
+import { Article } from '../model/Article';
+
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+describe('Article', () => {
+
+    describe('Get article body attachment', () => {
+
+        // Attachments 
+        const file2Attachment = new Attachment();
+        file2Attachment.Filename = 'file-2';
+        file2Attachment.Disposition = 'inline';
+
+        const file1Attachment = new Attachment();
+        file1Attachment.Filename = 'file-1';
+        file1Attachment.Disposition = 'inline';
+
+        const file1HTMLAttachment = new Attachment();
+        file1HTMLAttachment.Filename = 'file-1.html';
+        file1HTMLAttachment.Disposition = 'inline';
+
+        // Articles
+        const file2Article = new Article();
+        file2Article.Attachments = [file2Attachment];
+
+        const file1Article = new Article();
+        file1Article.Attachments = [file1Attachment];
+
+        const file1HTMLArticle = new Article();
+        file1HTMLArticle.Attachments = [file1HTMLAttachment];
+
+        const file2file1Article = new Article();
+        file2file1Article.Attachments = [file2Attachment, file1Attachment];
+
+        const file2file1HTMLArticle = new Article();
+        file2file1HTMLArticle.Attachments = [file2Attachment, file1HTMLAttachment];
+
+        it('should set the body attachment to file-2 if only file-2 is given', () => {
+            const article = new Article(file2Article);
+            expect(article.bodyAttachment).exist;
+            expect(article.bodyAttachment.Filename).equals('file-2');
+        });
+
+        it('should set the body attachment to file-2 if file-2 and file-1 is given', () => {
+            const article = new Article(file2file1Article);
+            expect(article.bodyAttachment).exist;
+            expect(article.bodyAttachment.Filename).equals('file-2');
+        });
+
+        it('should set the body attachment to file-2 if file-2 and file-1.html is given', () => {
+            const article = new Article(file2file1HTMLArticle);
+            expect(article.bodyAttachment).exist;
+            expect(article.bodyAttachment.Filename).equals('file-2');
+        });
+
+        it('should set the body attachment to file-1 if no file-2 and file-1 is given', () => {
+            const article = new Article(file1Article);
+            expect(article.bodyAttachment).exist;
+            expect(article.bodyAttachment.Filename).equals('file-1');
+        });
+
+        it('should set the body attachment to file-1.html if no file-2 and file-1.html is given', () => {
+            const article = new Article(file1HTMLArticle);
+            expect(article.bodyAttachment).exist;
+            expect(article.bodyAttachment.Filename).equals('file-1.html');
+        });
+    });
+
+});
