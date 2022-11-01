0ea8cfb385a333c8d55a7015c4ebc65861a4e7a2
David Hein <david.hein@cape-it.de>
2022-11-01 14:46:47 +0100

    KIX2018-7540 fixed search result count

--- src/frontend-applications/agent-portal/modules/search/webapp/components/search-module/component.ts
+++ src/frontend-applications/agent-portal/modules/search/webapp/components/search-module/component.ts
@@ -10,26 +10,34 @@
 import { ConfigurationType } from '../../../../../model/configuration/ConfigurationType';
 import { TableWidgetConfiguration } from '../../../../../model/configuration/TableWidgetConfiguration';
 import { WidgetConfiguration } from '../../../../../model/configuration/WidgetConfiguration';
+import { Context } from '../../../../../model/Context';
+import { IdService } from '../../../../../model/IdService';
 import { KIXObjectType } from '../../../../../model/kix/KIXObjectType';
 import { AbstractMarkoComponent } from '../../../../base-components/webapp/core/AbstractMarkoComponent';
 import { ContextService } from '../../../../base-components/webapp/core/ContextService';
+import { EventService } from '../../../../base-components/webapp/core/EventService';
+import { IEventSubscriber } from '../../../../base-components/webapp/core/IEventSubscriber';
 import { LabelService } from '../../../../base-components/webapp/core/LabelService';
 import { TranslationService } from '../../../../translation/webapp/core/TranslationService';
+import { SearchEvent } from '../../../model/SearchEvent';
 import { SearchContext } from '../../core';
 import { ComponentState } from './ComponentState';
 
 class Component extends AbstractMarkoComponent<ComponentState> {
+
+    private context: SearchContext;
+    private subscriber: IEventSubscriber;
 
     public onCreate(): void {
         this.state = new ComponentState();
     }
 
     public async onMount(): Promise<void> {
-        const context = ContextService.getInstance().getActiveContext<SearchContext>();
-        const searchCache = context.getSearchCache();
+        this.context = ContextService.getInstance().getActiveContext<SearchContext>();
+        const searchCache = this.context?.getSearchCache();
 
         this.state.icon = LabelService.getInstance().getObjectIconForType(searchCache.objectType);
-        this.state.title = await this.getTitle(searchCache.objectType);
+        await this.setTitle(searchCache.objectType);
 
         const widgetConfiguration = new WidgetConfiguration(
             'search-result-widget-' + searchCache.name, searchCache.name, ConfigurationType.TableWidget,
@@ -42,13 +50,31 @@ class Component extends AbstractMarkoComponent<ComponentState> {
         this.state.objectType = searchCache.objectType;
         this.state.configuration = widgetConfiguration;
 
+        this.subscriber = {
+            eventSubscriberId: IdService.generateDateBasedId(),
+            eventPublished: (context: Context): void => {
+                if (context.instanceId === this.context?.instanceId) {
+                    this.setTitle();
+                }
+            }
+        };
+
+        EventService.getInstance().subscribe(SearchEvent.SEARCH_CACHE_CHANGED, this.subscriber);
+
         this.state.prepared = true;
     }
 
-    private async getTitle(objectType: KIXObjectType | string): Promise<string> {
+    public onDestroy(): void {
+        EventService.getInstance().unsubscribe(SearchEvent.SEARCH_CACHE_CHANGED, this.subscriber);
+    }
+
+    private async setTitle(objectType: KIXObjectType | string = this.state.objectType): Promise<void> {
         const objectName = await LabelService.getInstance().getObjectName(objectType, true);
-        const title = await TranslationService.translate('Translatable#Search Results: {0}', [objectName]);
-        return title;
+        let title = await TranslationService.translate('Translatable#Search Results: {0}', [objectName]);
+
+        const resultCount = this.context?.getSearchCache()?.result?.length || 0;
+        title += ` (${resultCount})`;
+        this.state.title = title;
     }
 }
 
