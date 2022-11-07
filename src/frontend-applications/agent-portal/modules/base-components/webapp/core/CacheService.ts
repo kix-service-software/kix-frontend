0cb8d4cb6d149e2c1bec3c5eff930b754d456abf
David Hein <david.hein@cape-it.de>
2022-11-07 06:37:36 +0000

    KIX2018-8614 added cache dependency

--- src/frontend-applications/agent-portal/modules/base-components/webapp/core/CacheService.ts
+++ src/frontend-applications/agent-portal/modules/base-components/webapp/core/CacheService.ts
@@ -254,6 +254,9 @@ export class BrowserCacheService {
                 cacheKeyPrefixes.push(KIXObjectType.REPORT);
                 cacheKeyPrefixes.push(KIXObjectType.REPORT_RESULT);
                 break;
+            case KIXObjectType.WATCHER:
+                cacheKeyPrefixes.push(KIXObjectType.TICKET);
+                break;
             case KIXObjectType.ARTICLE:
                 cacheKeyPrefixes.push(KIXObjectType.ATTACHMENT);
                 cacheKeyPrefixes.push('Ticket-Article-Attachment');
