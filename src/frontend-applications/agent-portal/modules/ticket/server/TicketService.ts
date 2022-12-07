31b1b178eaad7e8bb3902a13b5138e07de39b4e4
David Hein <david.hein@cape-it.de>
2022-12-07 09:10:58 +0100

    KIX2018-8619 remove hook and divider for ticket number search

--- src/frontend-applications/agent-portal/modules/ticket/server/TicketService.ts
+++ src/frontend-applications/agent-portal/modules/ticket/server/TicketService.ts
@@ -50,6 +50,7 @@ import { SysConfigKey } from '../../sysconfig/model/SysConfigKey';
 import { UIComponentPermission } from '../../../model/UIComponentPermission';
 import { CRUD } from '../../../../../server/model/rest/CRUD';
 import { PermissionService } from '../../../server/services/PermissionService';
+import { SysConfigOption } from '../../sysconfig/model/SysConfigOption';
 
 export class TicketAPIService extends KIXObjectAPIService {
 
@@ -774,6 +775,18 @@ export class TicketAPIService extends KIXObjectAPIService {
             && lockCriteria.operator === SearchOperator.EQUALS
         ) {
             lockCriteria.value = lockCriteria.value[0];
+        }
+
+        const ticketNumberCriterion = searchCriteria.find((c) => c.property === TicketProperty.TICKET_NUMBER);
+        if (ticketNumberCriterion) {
+            const options = await SysConfigService.getInstance().loadObjects<SysConfigOption>(
+                token, 'TicketService', KIXObjectType.SYS_CONFIG_OPTION,
+                [SysConfigKey.TICKET_HOOK, SysConfigKey.TICKET_HOOK_DIVIDER], null, null
+            );
+
+            for (const o of options) {
+                ticketNumberCriterion.value = ticketNumberCriterion.value?.toString()?.replace(o.Value, '');
+            }
         }
 
         const hasStateSearch = searchCriteria.some((c) =>
