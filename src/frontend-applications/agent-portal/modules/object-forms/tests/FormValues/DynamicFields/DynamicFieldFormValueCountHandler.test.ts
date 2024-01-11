074b3da1b97c8c14deac2308d6a24d4fc684868c
Mario Bauer <mario.bauer@kixdesk.com>
2024-01-11 08:29:07 +0100

    KIX2018-10879: update copyrights

--- src/frontend-applications/agent-portal/modules/object-forms/tests/FormValues/DynamicFields/DynamicFieldFormValueCountHandler.test.ts
+++ src/frontend-applications/agent-portal/modules/object-forms/tests/FormValues/DynamicFields/DynamicFieldFormValueCountHandler.test.ts
@@ -8,7 +8,7 @@
  */
 
 // /**
-//  * Copyright (C) 2006-2022 c.a.p.e. IT GmbH, https://www.cape-it.de
+//  * Copyright (C) 2006-2024 KIX Service Software GmbH, https://www.kixdesk.com
 //  * --
 //  * This software comes with ABSOLUTELY NO WARRANTY. For details, see
 //  * the enclosed file LICENSE for license information (GPL3). If you
