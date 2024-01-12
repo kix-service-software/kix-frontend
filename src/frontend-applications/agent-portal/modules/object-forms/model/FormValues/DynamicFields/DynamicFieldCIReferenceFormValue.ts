09364dba562af914891f6bb419e8fd68f4fad1b4
Ricky Kaiser <ricky.kaiser@kixdesk.com>
2024-01-12 07:56:34 +0100

    KIX2018-10544: load selected object separately

--- src/frontend-applications/agent-portal/modules/object-forms/model/FormValues/DynamicFields/DynamicFieldCIReferenceFormValue.ts
+++ src/frontend-applications/agent-portal/modules/object-forms/model/FormValues/DynamicFields/DynamicFieldCIReferenceFormValue.ts
@@ -24,6 +24,7 @@ import { SelectObjectFormValue } from '../SelectObjectFormValue';
 
 export class DynamicFieldCIReferenceFormValue<T = number> extends SelectObjectFormValue<T> {
 
+    protected loadObjectsSeparately: boolean = true;
 
     public constructor(
         public property: string,
