ce7440a7a390c85f890684167a3f4a27c26d9228
Ricky Kaiser <ricky.kaiser@cape-it.de>
2024-06-26 11:40:34 +0200

    KIX2018-11674: consider first value change, get label if property required afterwards

--- src/frontend-applications/agent-portal/modules/base-components/webapp/components/dynamic-form-field-container/DynamicFormFieldValue.ts
+++ src/frontend-applications/agent-portal/modules/base-components/webapp/components/dynamic-form-field-container/DynamicFormFieldValue.ts
@@ -169,8 +169,7 @@ export class DynamicFormFieldValue {
 
         // get label (needed if field is required)
         if (this.required) {
-            const objectType = this.value.objectType ? this.value.objectType : this.manager.objectType;
-            this.label = await LabelService.getInstance().getPropertyText(this.value.property, objectType);
+            await this.setLabel();
         }
 
         await this.manager.setValue(this.value, silent);
@@ -180,6 +179,13 @@ export class DynamicFormFieldValue {
         await this.setRelativeTimeUnitTree();
         await this.setWithinTrees();
         await this.createValueInput();
+    }
+
+    public async setLabel(): Promise<void> {
+        if (!this.label) {
+            const objectType = this.value.objectType ? this.value.objectType : this.manager.objectType;
+            this.label = await LabelService.getInstance().getPropertyText(this.value.property, objectType);
+        }
     }
 
     public async setPropertyTree(): Promise<string> {
