33c254660e07f38ea8632dd36d214486b4bf2e11
Mario Bauer <mario.bauer@kixdesk.com>
2025-02-21 10:57:02 +0100

    KIX2018-11699: provide configuration handling for pattern

---
+++ src/frontend-applications/agent-portal/modules/job/webapp/components/job-input-attachmentPatternRule/component.ts
@@ -0,0 +1,111 @@
+/**
+ * Copyright (C) 2006-2024 KIX Service Software GmbH, https://www.kixdesk.com
+ * --
+ * This software comes with ABSOLUTELY NO WARRANTY. For details, see
+ * the enclosed file LICENSE for license information (GPL3). If you
+ * did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
+ * --
+ */
+
+import { ComponentState } from './ComponentState';
+import { FormInputComponent } from '../../../../base-components/webapp/core/FormInputComponent';
+import { TranslationService } from '../../../../translation/webapp/core/TranslationService';
+import { ContextService } from '../../../../base-components/webapp/core/ContextService';
+
+class Component extends FormInputComponent<[string, string, string], ComponentState> {
+
+    public onCreate(): void {
+        this.state = new ComponentState();
+    }
+
+    public onInput(input: any): void {
+        super.onInput(input);
+        this.update();
+    }
+
+    private async update(): Promise<void> {
+        this.state.labelPatternFilename = await TranslationService.translate('Translatable#Filename');
+        this.state.labelPatternContentType = await TranslationService.translate('Translatable#ContentType');
+        this.state.labelPatternDisposition = await TranslationService.translate('Translatable#Disposition');
+    }
+
+    public async onMount(): Promise<void> {
+        await super.onMount();
+        this.state.prepared = true;
+    }
+
+    public async setCurrentValue(): Promise<void> {
+        const context = ContextService.getInstance().getActiveContext();
+        const formInstance = await context?.getFormManager()?.getFormInstance();
+        const value = formInstance.getFormFieldValue<string>(this.state.field?.instanceId);
+        if (value && Array.isArray(value.value)) {
+            this.state.currentPatternFilename = value.value[0];
+            this.state.currentPatternContentType = value.value[1];
+            this.state.currentPatternDisposition = value.value[2];
+        }
+    }
+
+    public valueFilenameChanged(event: any): void {
+        if (event) {
+            this.state.currentPatternFilename = event.target && event.target.value !== '' ? event.target.value : null;
+            (this as any).emit(
+                'valueChanged',
+                [
+                    this.state.currentPatternFilename, this.state.currentPatternContentType,
+                    this.state.currentPatternDisposition
+                ]
+            );
+            super.provideValue(
+                [
+                    this.state.currentPatternFilename, this.state.currentPatternContentType,
+                    this.state.currentPatternDisposition
+                ]
+            );
+        }
+    }
+
+    public valueContentTypeChanged(event: any): void {
+        if (event) {
+            this.state.currentPatternContentType = event.target && event.target.value !== '' ? event.target.value : null;
+            (this as any).emit(
+                'valueChanged',
+                [
+                    this.state.currentPatternFilename, this.state.currentPatternContentType,
+                    this.state.currentPatternDisposition
+                ]
+            );
+            super.provideValue(
+                [
+                    this.state.currentPatternFilename, this.state.currentPatternContentType,
+                    this.state.currentPatternDisposition
+                ]
+            );
+        }
+    }
+
+    public valueDispositionChanged(event: any): void {
+        if (event) {
+            this.state.currentPatternDisposition = event.target && event.target.value !== '' ? event.target.value : null;
+            (this as any).emit(
+                'valueChanged',
+                [
+                    this.state.currentPatternFilename, this.state.currentPatternContentType,
+                    this.state.currentPatternDisposition
+                ]
+            );
+            super.provideValue(
+                [
+                    this.state.currentPatternFilename, this.state.currentPatternContentType,
+                    this.state.currentPatternDisposition
+                ]
+            );
+        }
+    }
+
+    public async focusLost(event: any): Promise<void> {
+        await super.focusLost();
+    }
+
+}
+
+module.exports = Component;
---
+++ src/frontend-applications/agent-portal/modules/ticket/webapp/core/form/extended-form-manager/AttachmentPatternRuleFieldsOptionFieldHandler.ts
@@ -0,0 +1,55 @@
+/**
+ * Copyright (C) 2006-2024 KIX Service Software GmbH, https://www.kixdesk.com
+ * --
+ * This software comes with ABSOLUTELY NO WARRANTY. For details, see
+ * the enclosed file LICENSE for license information (GPL3). If you
+ * did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
+ * --
+ */
+
+import { FormFieldConfiguration } from '../../../../../../model/configuration/FormFieldConfiguration';
+import { JobTypes } from '../../../../../job/model/JobTypes';
+import { MacroAction } from '../../../../../macro/model/MacroAction';
+import { MacroActionTypeOption } from '../../../../../macro/model/MacroActionTypeOption';
+import { OptionFieldHandler } from '../../../../../macro/model/OptionFieldHandler';
+
+export class AttachmentPatternRuleFieldsOptionFieldHandler extends OptionFieldHandler {
+
+    public async createOptionField(
+        action: MacroAction, option: MacroActionTypeOption, actionType: string, actionFieldInstanceId: string,
+        jobType: string
+    ): Promise<FormFieldConfiguration> {
+        const isRuleFieldOption = option.Name === 'KeepRules' || option.Name === 'DeleteRules';
+        const isRelevantMacroAction = actionType === 'ArticleAttachmentsDelete';
+
+        if (jobType === JobTypes.TICKET && isRuleFieldOption && isRelevantMacroAction) {
+            let defaultValue;
+            if (action && action.Parameters) {
+                defaultValue = action.Parameters[option.Name];
+            }
+            else if (option.DefaultValue) {
+                defaultValue = option.DefaultValue;
+            }
+
+            return this.getOptionField(
+                option, actionType, actionFieldInstanceId, 'job-input-attachmentPatternRule',
+                defaultValue, 1, 99, option.Name === 'DeleteRules' ? 1 : 0
+            );
+        }
+        return;
+    }
+
+    public postPrepareOptionValue(actionType: string, optionName: string, value: any, parameter: any): any {
+        const isRuleFieldOption = optionName === 'KeepRules' || optionName === 'DeleteRules';
+        const isRelevantMacroAction = actionType === 'ArticleAttachmentsDelete';
+
+        if (isRuleFieldOption && isRelevantMacroAction) {
+            if (Array.isArray(value) && value[0] !== '' && value[1] !== '' && value[2] !== '') {
+                return this.valueAsArray(parameter, optionName, value);
+            } else {
+                return parameter[optionName];
+            }
+        }
+        return;
+    }
+}
