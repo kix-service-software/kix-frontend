770972c7954fd2f1c4df7815dfb96b89e8dc17c0
Ricky Kaiser <ricky.kaiser@kixdesk.com>
2025-02-26 15:17:04 +0100

    KIX2018-13121: consider only body html

---
+++ src/frontend-applications/agent-portal/model/CheckUtil.ts
@@ -0,0 +1,17 @@
+/**
+ * Copyright (C) 2006-2025 KIX Service Software GmbH, https://www.kixdesk.com
+ * --
+ * This software comes with ABSOLUTELY NO WARRANTY. For details, see
+ * the enclosed file LICENSE for license information (GPL3). If you
+ * did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
+ * --
+ */
+
+export class CheckUtil {
+
+    public static isNumeric(testString: any): boolean {
+        return typeof testString === 'number' ||
+            (typeof testString === 'string' && Boolean(testString.match(/^\d+$/)));
+    }
+
+}
