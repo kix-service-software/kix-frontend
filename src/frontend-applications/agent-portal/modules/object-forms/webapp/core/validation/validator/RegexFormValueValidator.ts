997f5de077bf3b192b926e3f1a532510a89cceac
David Hein <david.hein@cape-it.de>
2025-03-03 09:35:20 +0100

    KIX2018-12715 append values for selection

--- src/frontend-applications/agent-portal/modules/object-forms/webapp/core/validation/validator/RegexFormValueValidator.ts
+++ src/frontend-applications/agent-portal/modules/object-forms/webapp/core/validation/validator/RegexFormValueValidator.ts
@@ -17,13 +17,20 @@ export class RegexFormValueValidator extends ObjectFormValueValidator {
     public async validate(formValue: ObjectFormValue<any>): Promise<ValidationResult[]> {
         const result = [];
 
-        if (formValue?.value && formValue?.regExList?.length) {
-            formValue.regExList.forEach((regExItem) => {
-                const regex = new RegExp(regExItem.regEx);
-                if (!regex.test(formValue.value)) {
-                    result.push(new ValidationResult(ValidationSeverity.ERROR, regExItem.errorMessage));
-                }
-            });
+        let selectedValues = formValue.value || [];
+        if (selectedValues && !Array.isArray(selectedValues)) {
+            selectedValues = [selectedValues];
+        }
+
+        if (selectedValues?.length && formValue?.regExList?.length) {
+            for (const value of selectedValues) {
+                formValue.regExList.forEach((regExItem) => {
+                    const regex = new RegExp(regExItem.regEx);
+                    if (!regex.test(value)) {
+                        result.push(new ValidationResult(ValidationSeverity.ERROR, regExItem.errorMessage));
+                    }
+                });
+            }
         }
 
         return result;
