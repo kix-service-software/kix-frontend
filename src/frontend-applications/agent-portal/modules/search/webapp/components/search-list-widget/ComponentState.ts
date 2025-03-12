06b0e150046eb77d964285b6aafbe0fe0287ceca
David Hein <GITLAB_USER_EMAIL>
2025-03-12 14:40:45 +0000

    [skip ci] updated copyright and licenses

--- src/frontend-applications/agent-portal/modules/search/webapp/components/search-list-widget/ComponentState.ts
+++ src/frontend-applications/agent-portal/modules/search/webapp/components/search-list-widget/ComponentState.ts
@@ -1,3 +1,12 @@
+/**
+ * Copyright (C) 2006-2025 KIX Service Software GmbH, https://www.kixdesk.com
+ * --
+ * This software comes with ABSOLUTELY NO WARRANTY. For details, see
+ * the enclosed file LICENSE for license information (GPL3). If you
+ * did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
+ * --
+ */
+
 import { AbstractComponentState } from '../../../../base-components/webapp/core/AbstractComponentState';
 import { TreeNode } from '../../../../base-components/webapp/core/tree';
 
