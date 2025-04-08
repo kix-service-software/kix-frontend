924f4c8b77a84163c7ff4c63ff32ea49d1a725cf
David Hein <david.hein@cape-it.de>
2025-04-08 14:36:25 +0200

    KIX2018-13600 added ContactFormService

---
+++ src/frontend-applications/agent-portal/modules/customer/webapp/core/form/ContactFormService.ts
@@ -0,0 +1,32 @@
+/**
+ * Copyright (C) 2006-2024 KIX Service Software GmbH, https://www.kixdesk.com
+ * --
+ * This software comes with ABSOLUTELY NO WARRANTY. For details, see
+ * the enclosed file LICENSE for license information (GPL3). If you
+ * did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
+ * --
+ */
+
+import { KIXObjectType } from '../../../../../model/kix/KIXObjectType';
+import { KIXObjectFormService } from '../../../../base-components/webapp/core/KIXObjectFormService';
+
+export class ContactFormService extends KIXObjectFormService {
+
+    private static INSTANCE: ContactFormService;
+
+    public static getInstance(): ContactFormService {
+        if (!ContactFormService.INSTANCE) {
+            ContactFormService.INSTANCE = new ContactFormService();
+        }
+        return ContactFormService.INSTANCE;
+    }
+
+    private constructor() {
+        super();
+    }
+
+    public isServiceFor(objectType: KIXObjectType): boolean {
+        return objectType === KIXObjectType.CONTACT;
+    }
+
+}
