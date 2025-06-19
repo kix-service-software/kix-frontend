b4e501923267740c10d564e2e8bb984f9205ecff
David Hein <david.hein@cape-it.de>
2025-06-19 13:03:45 +0200

    KIX2018-14113 optimized toast implementation

--- src/frontend-applications/agent-portal/modules/toast/webapp/core/ToastUtil.ts
+++ src/frontend-applications/agent-portal/modules/toast/webapp/core/ToastUtil.ts
@@ -16,15 +16,15 @@ export class ToastUtil {
     private static showRefreshToastTimeout: any;
 
     public static async showInfoToast(message: string): Promise<void> {
-        this.showToast('info-toast', { message }, { autohide: true, animation: true, delay: 2500 });
+        this.showToast('info-toast', { message });
     }
 
     public static async showErrorToast(message: string): Promise<void> {
-        this.showToast('error-toast', { message }, { autohide: false, animation: true, delay: 2500 });
+        this.showToast('error-toast', { message });
     }
 
     public static async showSuccessToast(message: string): Promise<void> {
-        this.showToast('success-toast', { message }, { autohide: true, animation: true, delay: 2500 });
+        this.showToast('success-toast', { message });
     }
 
     public static showRefreshToast(): void {
@@ -33,19 +33,19 @@ export class ToastUtil {
         }
 
         this.showRefreshToastTimeout = setTimeout(() => {
-            this.showToast('refresh-app-toast', {}, { autohide: false, animation: true });
+            this.showToast('refresh-app-toast', {});
         }, 200);
     }
 
-    private static showToast(toastId: string, toastData: any = {}, toastOptions: any = {}): void {
+    private static showToast(toastId: string, toastData: any = {}): void {
         const toastArea = document.getElementById('kix-toast-area');
         if (toastArea) {
             const template = KIXModulesService.getComponentTemplate(toastId);
             const content = template?.default?.renderSync(toastData);
             content.appendTo(toastArea);
 
-            const templateToast = new bootstrap.Toast(`#${toastId}`, toastOptions);
-            templateToast?.show();
+            const templateToast = new bootstrap.Toast(`#${toastId}`);
+            templateToast.show();
 
             const toastElement = document.getElementById(toastId);
             toastElement.addEventListener('hidden.bs.toast', (event) => {
